6bebe30c3f2dbd8185646bedae84afdba72a28c2
johnshields <g00348436@gmit.ie>
2021-03-21 20:14:22 +0000

    Clean up fcl.

--- game/ie/gmit/sw/ai/Runner.java
+++ game/ie/gmit/sw/ai/Runner.java
@@ -7,21 +7,6 @@ public class Runner {
 	public static FIS fis;
 
 	public static void main(String[] args) {
-		/*
-		 * PLEASE READ CAREFULLY
-		 * ---------------------
-		 * If you need to load FCL functions to the application or
-		 * train, configure and load a neural network, then it is 
-		 * best to do all of this before loading the UI. Launching
-		 * a UI in any language or framework and then starting a 
-		 * long running task in the same thread is guaranteed to
-		 * freeze the screen and crash the programme.
-		 * 
-		 * NB: you can assume that the JavaFX 15 API is already
-		 * available and configured on the MODULE-PATH (NOT THE 
-		 * CLASSPATH). 
-		 */
-
 		// Load and Parse FCL file
 		fis = FIS.load("resources/fuzzy/tempRadius.fcl", true);
 		if(fis == null ) {
@@ -29,12 +14,8 @@ public class Runner {
 			return;
 		}
 
-		/*
-		 * Launch the JavaFX UI only when all the long-running AI 
-		 * configuration tasks have been completed. Use the arrow 
-		 * keys to move the player character and the 'Z' key to 
-		 * toggle the zoom in / out.
-		 */
+		// train, configure and load a neural network HERE
+
 		Application.launch(GameWindow.class, args);
 	}
 }
